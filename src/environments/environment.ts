a053ef47b26b7f11474e5d4b2b0275789f226d28
Duncan <d.mcpherson.home@gmail.com>
2024-04-20 12:08:42 -0600

    more build testing

---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+import {IEnvironment} from "./environment.model";
+
+export const environment: IEnvironment = {
+	recaptchaKey: '6LdTzMEpAAAAABlAQMWzGcu1kEdJhYTRn-sx0-Ne',
+	firebaseConfig: {
+		apiKey: "AIzaSyDsQYoL5WYsICsFl31d5b_GFVWqVsJ4oyI",
+		authDomain: "date-idea-scrambler.firebaseapp.com",
+		databaseURL: "https://date-idea-scrambler-dev.firebaseio.com",
+		projectId: "date-idea-scrambler",
+		storageBucket: "date-idea-scrambler.appspot.com",
+		messagingSenderId: "761823723903",
+		appId: "1:761823723903:web:7cdba597ffbaa79d07da4a",
+		measurementId: "G-R7F04XYCJW"
+	}
+}
